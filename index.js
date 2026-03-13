7510917bc54e16ed3ee110e42a5c27ed068b9156
dunkledan <163540121+dunkledan@users.noreply.github.com>
2026-03-13 13:44:20 +0000

    v2.5
    website layout complete, need to do write up sections, and add all projects to the acctual website

--- index.js
+++ index.js
@@ -2,3 +2,23 @@ function goToAboutMe()
 {
     document.getElementById("AboutMe").scrollIntoView({behavior:"smooth"})
 }
+
+function goToTechSkills()
+{
+    document.getElementById("technicalSkills").scrollIntoView({behavior:"smooth"})
+}
+
+function goToProjects()
+{
+    location.replace("/Projects.html");
+}
+
+function GoToHome()
+{
+    location.replace("/index.html");
+}
+
+function DisplayContacts()
+{
+    document.getElementById("myDropdown").classList.toggle("show");
+}
