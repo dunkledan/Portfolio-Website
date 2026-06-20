42220c7f07fd505a58b77e66bd08a2c7fe9f2ea1
dunkledan <163540121+dunkledan@users.noreply.github.com>
2026-06-20 20:35:14 +0100

    v36.000002
    cool top scrolling demo videos

--- index.js
+++ index.js
@@ -78,3 +78,29 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+
+const track2 = document.getElementById("FlashyVideoDemoContainer");
+const videos = document.querySelectorAll(".flashy-video");
+
+let current = 0;
+
+function updateCarousel() 
+{
+    const width = videos[0].offsetWidth;
+    track2.style.transform = `translateX(-${current * width}px)`;
+    videos.forEach((video, index) => 
+    {
+        video.classList.toggle("active",index === current);
+    });
+}
+
+function smoothScroll() 
+{
+    current++;
+    if (current >= videos.length) 
+    {
+        current = 0;
+    }
+    updateCarousel();
+}
+updateCarousel();
